c865feaf5813d2670164d54330fa3871717ff06f
margaret_olivos07@outlook.com <margaret_olivos07@outlook.com>
2025-04-20 12:51:57 -0500

    prueba-mg

---
+++ src/main/java/com/serverest/qa/PruebaMg.java
@@ -0,0 +1,13 @@
+package com.serverest.qa;
+
+public class PruebaMg {
+    private int num1;
+    private int num2;
+
+    public int Suma(int num1, int num2) {
+        num1= this.num1;
+        num2= this.num2;
+
+        return num1+num2;
+    }
+}
